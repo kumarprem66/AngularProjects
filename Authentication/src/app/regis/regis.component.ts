d02d2e9ab71c269d2781cd6659c3a860dfd59d38
kumarprem66 <premstart10@gmail.com>
2023-08-23 15:33:37 +0530

    done authentication

---
+++ Authentication/src/app/regis/regis.component.ts
@@ -0,0 +1,39 @@
+import { Component,OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http'
+
+
+
+@Component({
+  selector: 'app-regis',
+  templateUrl: './regis.component.html',
+  styleUrls: ['./regis.component.css']
+})
+export class RegisComponent implements OnInit{
+
+  registrationForm: FormGroup;
+
+  constructor(private fb:FormBuilder,private http: HttpClient){
+    this.registrationForm = this.fb.group({
+      username:['',[Validators.required]],
+      password:['',[Validators.required]],
+      email:['',[Validators.required,Validators.email]],
+    })
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(){
+    if(this.registrationForm.valid){
+      const formData = this.registrationForm.value;
+
+      this.http.post('http://localhost:3000/user',formData).subscribe((response)=>{
+        console.log("Registration complted",response)
+        this.registrationForm.reset();
+      })
+    }
+  }
+
+}
